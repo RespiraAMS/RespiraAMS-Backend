6a2df3ce584065d4b2c6fed7919867f1df19cbad
KhaiNguyenHoang <khaiproject1234@gmail.com>
2026-09-10 09:29:37 +0700

    feat (apphost): postgres, k8s, redis and rabbitmq setting

--- src/Respira.AppHost/AppHost.cs
+++ src/Respira.AppHost/AppHost.cs
@@ -1,3 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+// Kubernetes settings
+var k8s = builder.AddKubernetesEnvironment("k8s");
+
+// Setup databases, messaging and cache
+var cache = builder.AddRedis("cache");
+var postgres = builder.AddPostgres("postgres").WithPgWeb().WithDataVolume();
+var rabbitmq = builder.AddRabbitMQ("rabbitmq").WithManagementPlugin();
+
 builder.Build().Run();
