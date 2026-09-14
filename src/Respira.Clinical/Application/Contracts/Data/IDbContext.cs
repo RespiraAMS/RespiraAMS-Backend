f065bb0d5f3a5ae58f0c695e3f8288914685f086
danglnh07 <danglnh07.work@gmail.com>
2026-09-14 21:23:43 +0700

    feat(clinical-diagnose): add db context

---
+++ src/Respira.Clinical/Application/Contracts/Data/IDbContext.cs
@@ -0,0 +1,62 @@
+using Microsoft.EntityFrameworkCore;
+using Respira.Domain.Entities;
+using Respira.ServiceDefaults.Models;
+
+namespace Respira.Application.Contracts.Data
+{
+    public interface IDbContext
+    {
+        DbSet<ClinicalVariable> ClinicalVariables { get; set; }
+        DbSet<Criterion> Criteria { get; set; }
+        DbSet<Pathogen> Pathogens { get; set; }
+        DbSet<RiskFactor> RiskFactors { get; set; }
+        DbSet<ScoreMetrics> ScoreMetrics { get; set; }
+        DbSet<ScoringRule> ScoringRules { get; set; }
+        DbSet<SuspectedCause> SuspectedCauses { get; set; }
+
+        /// <summary>
+        /// Save all changes to database
+        /// </summary>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>The number of records changed</returns>
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Execute all async db operations in transaction
+        /// </summary>
+        /// <param name="action">An async action that wrap around all database operations</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>Task</returns>
+        Task ExecuteInTransactionAsync(Func<Task> action, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Execute all db operations in transaction
+        /// </summary>
+        /// <param name="action">An action that wrap around all database operations</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>Task</returns>
+        Task ExecuteInTransactionAsync(Action action, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Stubs are ONLY used to establish join-table FKs, without the need to fetch the actual data from database
+        /// </summary>
+        /// <param name="id">ID of the item that get attached</param>
+        /// <typeparam name="T">Type of the item that get attached</typeparam>
+        /// <returns>The object of type T</returns>
+        T AttachStub<T>(Guid id) where T : Base;
+
+        /// <summary>
+        /// Helper method: used to bypass change tracking of EF Core when updating (adding) a list of IDs
+        /// to an entity (which basically adding new records in the join table)
+        /// </summary>
+        /// <param name="collection">The collection of the item to be added</param>
+        /// <param name="ids">The list of IDs to be added</param>
+        /// <typeparam name="T">Type of the item</typeparam>
+        void UpdateRelations<T>(ICollection<T> collection, IEnumerable<Guid>? ids) where T : Base;
+
+        /// <summary>
+        /// Dispose the context
+        /// </summary>
+        ValueTask DisposeAsync();
+    }
+}
---
+++ src/Respira.Clinical/Infrastructure/Data/ClinicalDbContext.cs
@@ -0,0 +1,175 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Respira.Application.Contracts.Data;
+using Respira.Domain.Entities;
+using Respira.ServiceDefaults.Models;
+
+namespace Respira.Infrastructure.Data
+{
+    public class ClinicalDbContext(DbContextOptions<ClinicalDbContext> options) : DbContext(options), IDbContext
+    {
+        private IExecutionStrategy GetExecutionStrategy() => base.Database.CreateExecutionStrategy();
+
+        public DbSet<ClinicalVariable> ClinicalVariables { get; set; }
+        public DbSet<Criterion> Criteria { get; set; }
+        public DbSet<Pathogen> Pathogens { get; set; }
+        public DbSet<RiskFactor> RiskFactors { get; set; }
+        public DbSet<ScoreMetrics> ScoreMetrics { get; set; }
+        public DbSet<ScoringRule> ScoringRules { get; set; }
+        public DbSet<SuspectedCause> SuspectedCauses { get; set; }
+
+        public async Task ExecuteInTransactionAsync(Func<Task> action, CancellationToken cancellationToken = default)
+        {
+            var strategy = GetExecutionStrategy();
+            await strategy.ExecuteAsync(
+                action,
+                async (ctx, op, token) =>
+                {
+                    await using var transaction = await ctx.Database
+                        .BeginTransactionAsync(token);
+
+                    try
+                    {
+                        await op();
+                        await ctx.SaveChangesAsync(token);
+                        await transaction.CommitAsync(token);
+                    }
+                    catch
+                    {
+                        await transaction.RollbackAsync(token);
+                        throw;
+                    }
+
+                    return true;
+                },
+                null,
+                cancellationToken);
+        }
+
+        public async Task ExecuteInTransactionAsync(Action action, CancellationToken cancellationToken = default)
+        {
+            var strategy = GetExecutionStrategy();
+            await strategy.ExecuteAsync(
+                action,
+                async (ctx, op, token) =>
+                {
+                    await using var transaction = await ctx.Database
+                        .BeginTransactionAsync(token);
+
+                    try
+                    {
+                        op();
+                        await ctx.SaveChangesAsync(token);
+                        await transaction.CommitAsync(token);
+                    }
+                    catch
+                    {
+                        await transaction.RollbackAsync(token);
+                        throw;
+                    }
+
+                    return true;
+                },
+                null,
+                cancellationToken);
+        }
+
+        public T AttachStub<T>(Guid id) where T : Base
+        {
+            // If the entity has been tracked by EF Core
+            var tracked = Set<T>().Local.FirstOrDefault(x => x.Id == id);
+            if (tracked is not null)
+            {
+                return tracked;
+            }
+
+            // Create a stub object (fake object that just has the ID) -> save memory
+            var stub = Activator.CreateInstance<T>();
+            stub.Id = id;
+            Set<T>().Attach(stub);
+            return stub;
+        }
+
+        public void UpdateRelations<T>(ICollection<T> collection, IEnumerable<Guid>? ids) where T : Base
+        {
+            if (ids == null) return;
+            var newIds = ids.ToHashSet();
+
+            // Remove items no longer in the list
+            foreach (var item in collection.Where(x => !newIds.Contains(x.Id)).ToList())
+            {
+                collection.Remove(item);
+            }
+
+            // Add new items only
+            var existingIds = collection.Select(x => x.Id).ToHashSet();
+            foreach (var id in newIds.Where(id => !existingIds.Contains(id)))
+            {
+                collection.Add(AttachStub<T>(id));
+            }
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Apply query filter to soft delete items
+            modelBuilder.Entity<ClinicalVariable>().HasQueryFilter(x => !x.IsDeleted);
+            modelBuilder.Entity<Criterion>().HasQueryFilter(x => !x.IsDeleted);
+            modelBuilder.Entity<Pathogen>().HasQueryFilter(x => !x.IsDeleted);
+            modelBuilder.Entity<RiskFactor>().HasQueryFilter(x => !x.IsDeleted);
+            modelBuilder.Entity<ScoreMetrics>().HasQueryFilter(x => !x.IsDeleted);
+            modelBuilder.Entity<ScoringRule>().HasQueryFilter(x => !x.IsDeleted);
+            modelBuilder.Entity<SuspectedCause>().HasQueryFilter(x => !x.IsDeleted);
+
+            // Config on clinical variable
+            modelBuilder.Entity<ClinicalVariable>().ToTable("clinical_variables");
+            modelBuilder.Entity<ClinicalVariable>().Property(x => x.ValueType).HasConversion<string>();
+
+            modelBuilder.Entity<Criterion>().ToTable("criteria");
+            modelBuilder.Entity<Criterion>().Ignore(x => x.Variables);
+            modelBuilder.Entity<Criterion>().OwnsOne(x => x.Formula, builder => builder.ToJson());
+
+            // Config on pathogen
+            modelBuilder.Entity<Pathogen>().ToTable("pathogens");
+
+            // Config on risk factor
+            modelBuilder.Entity<RiskFactor>().ToTable("risk_factors");
+            modelBuilder.Entity<RiskFactor>()
+                .HasOne(x => x.Pathogen)
+                .WithMany()
+                .HasForeignKey(x => x.PathogenId);
+            modelBuilder.Entity<RiskFactor>()
+                .HasOne(x => x.Criterion)
+                .WithMany()
+                .HasForeignKey(x => x.CriterionId);
+            modelBuilder.Entity<RiskFactor>().Ignore(x => x.Variables);
+
+            // Config on score metrics
+            modelBuilder.Entity<ScoreMetrics>().ToTable("score_metrics");
+
+            // Config on scoring rule
+            modelBuilder.Entity<ScoringRule>().ToTable("scoring_rules");
+            modelBuilder.Entity<ScoringRule>()
+                .HasOne(x => x.ScoreMetrics)
+                .WithMany(x => x.ScoringRules)
+                .HasForeignKey(x => x.ScoreMetricsId);
+            modelBuilder.Entity<ScoringRule>()
+                .HasOne(x => x.Criterion)
+                .WithMany()
+                .HasForeignKey(x => x.CriterionId);
+            modelBuilder.Entity<ScoringRule>().Ignore(x => x.Variables);
+            modelBuilder.Entity<ScoringRule>().OwnsOne(x => x.ScoreFunction, builder => builder.ToJson());
+        }
+
+        public override async ValueTask DisposeAsync()
+        {
+            await base.DisposeAsync();
+        }
+    }
+}
